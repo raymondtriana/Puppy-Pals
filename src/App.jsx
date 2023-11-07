b783db7b1ff282f124c75f0e49d189be3061364f
raymond triana <raymondtriana@gmail.com>
2023-11-07 15:40:59 -0500

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import { puppyList } from "./data";
+import "./App.css";
+
+function App() {
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupID, setFeatPupID] = useState(null);
+  
+  const featuredPup = puppies.find((puppy)=>puppy.id === featPupID);
+  console.log(featuredPup);
+  console.log(puppies);
+  return (
+    <div>
+      {featPupID && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
+      {puppies.map((puppy) => (
+        <p
+          onClick={() => {
+            setFeatPupID(puppy.id);
+          }}
+          key={puppy.id}
+        >
+          {puppy.name}
+        </p>
+      ))}
+    </div>
+  );
+}
+
+export default App;
